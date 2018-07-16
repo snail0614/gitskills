6f1712fa9576b83139bf245e8ba3ca270ce99bfc
snail0614 <986832088@qq.com>
2018-07-16 01:16:28 -0700

    add mythreadpool.c mythreadpool.h

---
+++ threadpool/mythreadpool/mythreadpool.c
@@ -0,0 +1,367 @@
+/*************************************************************************
+	> File Name: threadpool.c
+	> Author: xkk
+	> Mail: xukunkun0614@163.com 
+	> Created Time: Thu 21 Jun 2018 03:41:04 PM PDT
+ ************************************************************************/
+
+#include"mythreadpool.h"
+
+/*****************全局变量定义********************/
+
+thread_pool		*g_pool = NULL;					/*线程池*/
+thread_revoke	*g_thread_revoke = NULL;			/*线程取消*/
+int				g_max_thread_num = 50;			/*最大线程数*/
+int				g_min_thread_num = 3;			/*最小线程数*/
+int				g_def_thread_num = 25;			/*默认线程数*/
+int				g_manage_adjust_interval = 5;	/*管理线程函数调整时间间隔*/
+int				g_work_thread_high_ratio = 3;	/*任务与线程峰值比例*/
+int				g_work_thread_low_ratio = 1;	/*任务与线程低谷比例*/
+
+void pool_init(int thread_num){
+int i;
+	if(thread_num > g_max_thread_num)
+		thread_num = g_max_thread_num;
+	if(thread_num < g_min_thread_num)
+		thread_num = g_min_thread_num;
+
+pthread_attr_t attr;
+	if(pthread_attr_init(&attr)){
+		perror("pthread_attr-init");
+		exit(1);
+	}
+	if(pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED)){
+		perror("pthread_attr_setdetachstate");
+		exit(1);
+	}
+
+	g_pool = (thread_pool *)malloc(sizeof(thread_pool));
+	pthread_mutex_init(&(g_pool->thread_queue_lock),NULL);
+	pthread_mutex_init(&(g_pool->thread_queue_remove_lock),NULL);
+	pthread_cond_init(&(g_pool->work_queue_ready),NULL);
+	pthread_mutex_init(&(g_pool->thread_run_lock),NULL);
+	pthread_mutex_init(&(g_pool->work_queue_lock),NULL);
+
+	g_pool->queue_work = NULL;
+	g_pool->thread_num = 0;
+	g_pool->thread_queue = NULL;
+	g_pool->queue_work_num = 0;
+	g_pool->shutdown = 0;
+
+	int temp;
+	for(i = 0; i < thread_num; i++){
+		g_pool->thread_num = i+1;
+		pthread_t thread_id;
+		pthread_create(&thread_id,&attr,thread_run,NULL);
+		thread_queue_add_thread(&(g_pool->thread_queue),thread_id,&temp);
+		printf("线程初始化……，正在创建第%d个线程,目前线程池中有%d个线\n",temp,g_pool->thread_num);
+
+	}
+	pthread_attr_destroy(&attr);
+
+}
+
+
+
+void thread_revoke_init(){
+	g_thread_revoke = (thread_revoke*)malloc(sizeof(thread_revoke));
+
+	g_thread_revoke->is_revoke = 0;
+	g_thread_revoke->revoke_count = 0;
+	g_thread_revoke->revoke_num = 0;
+}
+
+
+int thread_queue_add_thread(p_thread_node *thread_queue,pthread_t thread_id,int *count){
+	pthread_mutex_lock(&(g_pool->thread_queue_lock));
+thread_node *new_node = (p_thread_node *)malloc(sizeof(p_thread_node));
+	if(NULL == new_node){
+		printf("malloc for new thread queue node faile\n");
+		pthread_mutex_unlock(&(g_pool->thread_queue_lock));
+		return 1;
+	}
+
+	new_node->thread_id = thread_id;
+	new_node->next = NULL;
+
+	if(NULL == *(thread_queue)){
+		*(thread_queue) = new_node;
+		(*count)++;
+		pthread_mutex_unlock(&(g_pool->thread_queue_lock));
+		return 0;
+	}
+
+
+thread_node *p = *thread_queue;
+	new_node->next = p;
+	*(thread_queue) = new_node;
+	(*count)++;
+	pthread_mutex_unlock(&(g_pool->thread_queue_lock));
+	return 0;
+
+}
+
+
+int thread_queue_remove_node(p_thread_node *thread_queue, pthread_t thread_id,int *count){
+	pthread_mutex_lock(&(g_pool->thread_queue_remove_lock));
+p_thread_node current_node,pre_node;
+	if(NULL == *(thread_queue)){
+		printf("revoke a thread node from queue failed\n");
+		pthread_mutex_unlock(&(g_pool->thread_queue_remove_lock));
+		return 1;
+	}
+
+	current_node = *(thread_queue);
+	pre_node = *(thread_queue);
+int i = 1;
+	while(i < g_pool->thread_num && current_node != NULL){
+		i++;
+		if(thread_id == current_node->thread_id){
+		//	printf("线程 %d发现 ,删除\n",thread_id);
+			break;
+		}
+		pre_node = current_node;
+		current_node = current_node->next;
+	}
+
+	if(NULL == current_node){
+		pthread_mutex_unlock(&(g_pool->thread_queue_remove_lock));
+		return 1;
+	}
+
+	if(current_node->thread_id == (*(thread_queue))->thread_id){
+		*(thread_queue) = (*(thread_queue))->next;
+		free(current_node);
+		(*count)--;
+		pthread_mutex_unlock(&(g_pool->thread_queue_remove_lock));
+		return 0;
+	}
+
+	if(current_node->next == NULL){
+		pre_node->next = NULL;
+		free(current_node);
+		(*count)--;
+		pthread_mutex_unlock(&(g_pool->thread_queue_remove_lock));
+		return 0;
+	}
+
+	pre_node->next = current_node->next;
+	free(current_node);
+	(*count)--;
+	pthread_mutex_unlock(&(g_pool->thread_queue_remove_lock));
+	return 0;
+}
+
+int pool_add_work(void*(*process)(int arg),int arg){
+work_node *new_work = (work_node *)malloc(sizeof(work_node));
+	if(new_work == NULL){
+		return -1;
+	}			
+	
+	new_work->process = process;
+	new_work->arg = arg;
+	new_work->next = NULL;
+
+	pthread_mutex_lock(&(g_pool->work_queue_lock));
+	
+work_node *member = g_pool->queue_work;
+	if(NULL != member){			//尾插法
+		while(NULL != member->next){
+			member = member->next;
+		}
+		member->next = new_work;
+	}
+	else{
+		g_pool->queue_work = new_work;
+	}
+	assert(g_pool->queue_work != NULL);
+	g_pool->queue_work_num++;
+	pthread_mutex_unlock(&(g_pool->work_queue_lock));
+	pthread_cond_signal(&(g_pool->work_queue_ready));
+	return 0;
+}
+
+
+void pool_add_thread(int thread_num){
+int i;
+pthread_attr_t attr;
+	if(pthread_attr_init(&attr)){
+		perror("pthread_attr_init err\n");
+		exit(1);
+	}
+
+	if(pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED)){
+		perror("pthread_attr_setdetachstate err\n");
+		exit(1);
+	}
+
+	for(i = 0;i < thread_num; i++){
+		pthread_t thread_id;
+		g_pool->thread_num ++;
+		pthread_create(&thread_id,&attr,thread_run,NULL);
+		printf("新建线程 %u,目前线程池中有%d个线程\n",thread_id,g_pool->thread_num);
+		thread_queue_add_thread(&(g_pool->thread_queue),thread_id,&(g_pool->thread_num));
+	}
+
+	pthread_attr_destroy(&attr);
+}
+
+
+void pool_revoke_thread(int revoke_thread_num){
+	if(revoke_thread_num == 0){
+		return;
+	}
+	g_thread_revoke->revoke_num = revoke_thread_num;
+	g_thread_revoke->is_revoke = 1;
+	pthread_cond_broadcast(&(g_pool->work_queue_ready));
+}
+
+void *thread_manage(void *arg){
+int optvalue;
+int thread_num;
+	while(1){
+		if(g_pool->queue_work_num > g_work_thread_high_ratio * g_pool->thread_num){
+			optvalue = 1;
+			thread_num = 2 * g_pool->queue_work_num /(g_work_thread_high_ratio
+					+ g_work_thread_low_ratio) - g_pool->thread_num;
+/*			thread_num = (g_pool->queue_work_num - g_work_thread_high_ratio * 
+					g_pool->thread_num) / g_work_thread_high_ratio;*/
+		}
+		else if(g_pool->queue_work_num < g_work_thread_low_ratio * g_pool->thread_num){
+			optvalue = 2;
+			thread_num = g_pool->thread_num - 2 * g_pool->queue_work_num / 
+				(g_work_thread_high_ratio + g_work_thread_low_ratio);
+
+		}
+		if(1 == optvalue){
+			if(g_pool->thread_num + thread_num > g_max_thread_num){
+				thread_num = g_max_thread_num - g_pool->thread_num;
+			}
+			pool_add_thread(thread_num);
+		}
+		if(2 == optvalue){
+			if(g_min_thread_num > g_pool->thread_num - thread_num ){
+				thread_num = g_pool->thread_num - g_min_thread_num;
+			}
+			pool_revoke_thread(thread_num);
+		}
+	sleep(g_manage_adjust_interval);
+	}
+}
+
+
+int pool_destroy(){
+	if(g_pool->shutdown){
+		return -1;
+	}
+	g_pool->shutdown = 1;
+
+	pthread_cond_broadcast(&(g_pool->work_queue_ready));
+
+p_thread_node *q = g_pool->thread_queue;
+p_thread_node *p = q;
+
+	g_pool->thread_queue = NULL;
+
+	work_node *head = NULL;
+
+	while(g_pool->queue_work != NULL){
+		head = g_pool->queue_work;
+		g_pool->queue_work = g_pool->queue_work->next;
+		free(head);
+	}
+	g_pool->queue_work = NULL;
+
+	pthread_mutex_destroy(&(g_pool->thread_queue_lock));
+	pthread_mutex_destroy(&(g_pool->thread_run_lock));
+	pthread_mutex_destroy(&(g_pool->work_queue_lock));
+	pthread_mutex_destroy(&(g_pool->thread_queue_remove_lock));
+	pthread_cond_destroy(&(g_pool->work_queue_ready));
+
+	free(g_pool);
+	g_pool = NULL;
+}
+
+void *thread_run(void *arg){
+	while(1){
+		pthread_mutex_lock(&(g_pool->thread_run_lock));
+		while(g_thread_revoke->is_revoke == 0 && g_pool->queue_work_num == 0 && !g_pool->shutdown){
+			pthread_cond_wait(&(g_pool->work_queue_ready),&(g_pool->thread_run_lock));
+		}
+		/*如果销毁线程池，删除所有线程*/
+		if(g_pool->shutdown){
+			thread_queue_remove_node(&(g_pool->thread_queue),pthread_self(),&(g_pool->thread_num));
+			pthread_mutex_unlock(&(g_pool->thread_run_lock));
+			pthread_exit(NULL);
+			continue;
+		}
+		/*删除该线程*/
+		if(g_thread_revoke->is_revoke != 0 && g_thread_revoke->revoke_count < g_thread_revoke->revoke_num){
+
+			thread_queue_remove_node(&(g_pool->thread_queue),pthread_self(),&(g_pool->thread_num));
+			g_thread_revoke->revoke_count++;
+			if(g_thread_revoke->revoke_count == g_thread_revoke->revoke_num){
+				g_thread_revoke->is_revoke = 0;
+			}
+			pthread_mutex_unlock(&(g_pool->thread_run_lock));
+			printf("从队列删除成功, 线程 %u 退出,目前线程池中有%d个线程\n",pthread_self(),g_pool->thread_num);
+			pthread_exit(NULL);
+			continue;
+		}
+		/*回调函数，执行任务*/
+		if(g_pool->queue_work_num != 0){	
+		//	printf("线程 %u is starting to work \n",pthread_self());
+			assert(g_pool->queue_work_num != 0);
+			assert(g_pool->queue_work != NULL);
+			g_pool->queue_work_num--;
+work_node *work = g_pool->queue_work;
+			g_pool->queue_work = work->next;
+			pthread_mutex_unlock(&(g_pool->thread_run_lock));
+			(*(work->process))(work->arg);
+			free(work);
+			work = NULL;
+			continue;
+		}
+
+		pthread_mutex_unlock(&(g_pool->thread_run_lock));
+	}
+	pthread_exit(NULL);
+}
+
+void *myprocess(int arg){
+	//printf("线程 %u, working on task%d\n",pthread_self(),arg);
+	printf("线程 %u ***********执行任务%d中……\n",pthread_self(),arg);
+	sleep(1);
+	return NULL;
+}
+
+int main(int argc,char *argv[]){
+pthread_t manage_tid;
+	thread_revoke_init();
+	sleep(1);
+	pool_init(g_def_thread_num);
+	sleep(3);
+	pthread_create(&manage_tid,NULL,thread_manage,NULL);
+
+int i;
+	for(i = 0;  ; i++){
+		printf("******************添加任务%d*******************\n",i);
+		pool_add_work(myprocess,i);
+	//	sleep(1);
+		if(i % 20 == 0){
+			sleep(1);
+		}else if(i % 10 == 0){
+			sleep(2);
+		}
+
+	}
+	
+	sleep(5);
+	pool_dextroy();
+	return 0;
+
+}
+
+
+
+
---
+++ threadpool/mythreadpool/mythreadpool.h
@@ -0,0 +1,113 @@
+/*************************************************************************
+	> File Name: mythreadpool.h
+	> Author: xkk
+	> Mail: xukunkun0614@163.com 
+	> Created Time: Thu 21 Jun 2018 02:51:48 PM PDT
+ ************************************************************************/
+
+#ifndef _MYTHREADPOOL_
+#define _MYTHREADPOOL_
+
+#include<stdio.h>
+#include<stdlib.h>		//包含了的C语言标准库函数的定义。定义了五种类型、一些宏和通用工具函数，
+						//例如size_t、宏例如EXIT_FAILURE、EXIT_SUCCESS、RAND_MAX等。
+#include<unistd.h>		//unistd.h: unix std，是POSIX标准定义的unix类系统定义符号常量的头文件，
+						//包含了许多UNIX系统服务的函数原型，例如read函数、write函数和getpid函数。
+#include<sys/types.h>	//在应用程序源文件中包含<sys/types.h> 以访问 _LP64 和 _ILP32的定义。
+#include<pthread.h>
+#include<assert.h>
+
+/*******************结构体声明********************/
+
+/*
+ * 工作队列节点
+ */
+typedef struct _work{
+	void *(*process)(int arg);
+	int arg;
+	struct _work *next;
+}work_node;
+
+/*
+ *线程队列结节
+ * 
+ * */
+
+typedef struct _thread_queue_node{
+	pthread_t thread_id;
+	struct _thread_queue_node *next;
+}thread_node, *p_thread_node;
+
+
+/*
+ *线程池
+ *
+ */
+
+typedef struct {
+	int shutdown;
+	pthread_mutex_t	thread_queue_lock;
+	pthread_mutex_t	thread_run_lock;
+	pthread_mutex_t	work_queue_lock;
+	pthread_mutex_t thread_queue_remove_lock;
+	pthread_cond_t	work_queue_ready;
+	thread_node *thread_queue;
+	int thread_num;
+	int queue_work_num;
+	work_node *queue_work;
+}thread_pool;
+
+
+/**
+ *线程取消
+ */
+typedef struct {
+	int is_revoke;
+	int revoke_count;
+	int revoke_num;
+}thread_revoke;
+
+
+
+/*******************函数声明********************/
+
+/*向函数中添加任务*/
+int pool_add_work(void* (*process)(int arg),int arg);
+
+/*创建线程执行函数*/
+void *thread_run(void *arg);
+
+/*初始化线程池*/
+void pool_init(int thread_num);
+
+/*初始化撤销信息*/
+void thread_revoke_init();  
+
+/*销毁线程池*/
+int pool_destroy();
+
+/*向线程池中追加线程*/
+void pool_add_thread(int add_thread_num);
+
+/*向线程队列中追加线程*/
+int thread_queue_add_thread(p_thread_node *thread_queue,pthread_t thread_id,int *count);
+
+/*撤销线程*/
+void pool_revoke_thread(int revoke_thread_num);
+
+/*从线程队列中删除线程*/
+int thread_queue_remove_node(p_thread_node *thread_queue, pthread_t thread_id, int *remove_num);
+
+
+/*******************全局变量声明********************/
+
+extern thread_pool		*g_pool;					/*线程池*/
+extern thread_revoke	*g_thread_revoke;			/*线程取消*/
+extern int				g_max_thread_num;			/*最大线程数*/
+extern int				g_min_thread_num;			/*最小线程数*/
+extern int				g_def_thread_num;			/*默认线程数*/
+extern int				g_manage_adjust_interval;	/*管理线程函数调整时间间隔*/
+extern int				g_work_thread_high_ratio;	/*任务与线程峰值比例*/
+extern int				g_work_thread_low_ratio;	/*任务与线程低谷比例*/
+
+#endif
